054ecf6e0e789505767d6a97ed7de5d2728d7a83
Matthew Miller <mnmiller1@me.com>
2016-08-25 17:32:18 +1000

    Added checks for rotations required, need testing with a working robot

--- main.m
+++ main.m
@@ -6,7 +6,7 @@ function main
     global is_down;
     is_down = false;
     
-    points = [0 0; 500 500; 1 3; 500 500; 0 0];
+    points = [380 80; 20 80; 20 380; 320 320; 280 380];
     
     global motorA;
     motorA = NXTMotor('A');
@@ -19,54 +19,73 @@ function main
     global motorC;
     motorC = NXTMotor('C');
     motorC.ResetPosition();
+        
+    %StartDrawing();
     
-    StartDrawing();
-    
-    for x = points(:)
-        if isequal(x, [500, 500])
-            if (is_down)
-                StopDrawing();
-            else
-                StartDrawing();
-            end
-        else
+    for x = points
+        %if isequal(x, [10000, 10000])
+        %    if (is_down)
+        %        StopDrawing();
+        %    else
+        %        StartDrawing();
+        %    end
+        %else
             HandToPosition(x); 
-        end
+        %end
     end
     
-    StopDrawing();
+    %StopDrawing();
     
     COM_CloseNXT('all')
 end
 
 function HandToPosition(position)
-    distance_a = 83;
-    distance_b = 92;
+    % Hardcoded distances of each joint
+    distance_a = 75;
+    distance_b = 200; % This one still needs measuring.
     
-    x = position(1);
-    y = position(2);
+    % Grab the positions we want to go to.
+    x = position(1) - 200;
+    y = position(2) - 200;
     
-    Disp(strcat(strcat(num2str(x), ' '), num2str(y)));
+    % Grab the `D` value. D = (x_c^2 + y_c^2 - a_1^2 - a_2^2)/(2.a_1.a_2)
+    d = (x^2 + y^2 - distance_a^2 - distance_b^2) / (2 * distance_a * distance_b);
     
-    d = (x*x + y*y - distance_a * distance_a - distance_b * distance_b) / (2 * distance_a * distance_b);
-    theta_2 = atan(d, sqrt(1-(d*d)));
+    d
     
+    % Grab theta 2, which is atan2(d, sqrt(1 - d^2)) - If this returns div
+    % by zero, it's not possible to reach that spot.
+    theta_2 = atan2(d, sqrt(1 - d^2));
+    
+    % Multiply the cos and sin of theta 2 by the distance of joint b.
     tx = cos(theta_2) * distance_b;
     ty = sin(theta_2) * distance_b;
     
     theta_1 = atan2(x, y) - atan2(distance_a - tx, ty);
     
-    Disp(num2str(theta_1) + ' ' + num2str(theta_2));
-    
     global motorA;
-    motorA.TachoLimit = 360;
-    motorA.Power = 25;
+    data = motorA.ReadFromNXT();
+    motorA.TachoLimit = round(abs(data.Position - theta_1) * 180 / pi);
+    motorA.TachoLimit
+    if (data.Position > theta_1)
+        motorA.Power = 50;
+    else
+        motorA.Power = -50;
+    end    
     motorA.SendToNXT();
+    motorA.WaitFor();
     
     global motorB;
-    motorB.TachoLimit = 90;
-    motorB.Power = 0;
+    data = motorB.ReadFromNXT();
+    motorB.TachoLimit = round(abs(data.Position - theta_2) * 180 / pi);
+    motorB.TachoLimit
+    if (data.Position > theta_2)
+        motorB.Power = 50;
+    else
+        motorB.Power = -50;
+    end    
     motorB.SendToNXT();
+    %motorB.WaitFor();
 end
 
 function StartDrawing
